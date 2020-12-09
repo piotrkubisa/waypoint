bcc2fd6fa56d9086977b03ad7ddf255fd23e850f
Rae Krantz <8461333+krantzinator@users.noreply.github.com>
2020-12-08 19:40:50 -0500

    internal/cli: setting base for server uninstall

--- internal/serverinstall/serverinstall.go
+++ internal/serverinstall/serverinstall.go
@@ -13,8 +13,10 @@ import (
 // Installer is implemented by the server platforms and is responsible for managing
 // the installation of the Waypoint server.
 type Installer interface {
-	Install(ctx context.Context, ui terminal.UI, log hclog.Logger) (*clicontext.Config, *pb.ServerConfig_AdvertiseAddr, string, error)
+	Install(context.Context, terminal.UI, hclog.Logger) (*clicontext.Config, *pb.ServerConfig_AdvertiseAddr, string, error)
 	InstallFlags(*flag.Set)
+	Uninstall(context.Context, terminal.UI, hclog.Logger) error
+	UninstallFlags(*flag.Set)
 }
 
 var Platforms = map[string]Installer{
